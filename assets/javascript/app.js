c6a28429fb774c5fe702bffe0ea0fdfd56797df4
thescottbob <scottmcmains@gmail.com>
2018-09-14 10:31:55 -0700

    JSHW2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,23 @@
 //Declare variables for the Movie section questions, options, and answers
 var movieQuestion1 = "'It does not do to dwell on dreams and forget to live'"
-var movieQuestion1options = ["The Lord of The Rings: The Fellowship of The Ring", "Harry Potter and the Sorceror's Stone", "Moana", "Aladdin"];
+// var movieQuestion1options = ["The Lord of The Rings: The Fellowship of The Ring", "Harry Potter and the Sorceror's Stone", "Moana", "Aladdin"];
 var movieQuestion1correctAnswer = "Harry Potter and the Sorceror's Stone"
 
 var movieQuestion2 =  "'As long as you can still grab a breath, you fight. You breathe...keep breathing'";
-var movieQuestion2options = ["The Matrix", "Gladiator", "The Revenant", "Batman Begins"];
+// var movieQuestion2options = ["The Matrix", "Gladiator", "The Revenant", "Batman Begins"];
 var movieQuestion2correctAnswer = "The Revenant"
 
 var movieQuestion3 =  "'According to the map, we've only gone 4 inches'";
-var movieQuestion3options = ["Harold & Kumar Go To White Castle", "Borat", "Dumb and Dumber", "Anchorman"];
+// var movieQuestion3options = ["Harold & Kumar Go To White Castle", "Borat", "Dumb and Dumber", "Anchorman"];
 var movieQuestion3correctAnswer = "Dumb and Dumber"
 
 var movieQuestion4 =  "'We're not so different, you and I'";
-var movieQuestion4options = ["Spider Man", "Man of Steel", "Austin Powers: Goldmember", "Zoolander"];
+// var movieQuestion4options = ["Spider Man", "Man of Steel", "Austin Powers: Goldmember", "Zoolander"];
 var movieQuestion4correctAnswer = "Austin Powers: Goldmember"
 
 //If the 'Books' button is pressed, do the following 
 $(document).ready(function() {
+  $("ul").hide();
   $("#books").click(function(){
     $("#p2").hide();
     $("#books").hide();
@@ -28,16 +29,14 @@ $(document).ready(function() {
 
 //If the 'Movies' button is pressed, do the following 
 $(document).ready(function() {
+  $("ul").hide();
   $("#movies").click(function(){
     $("#p2").hide();
+    $("ul").show();
     $("#books").html(movieQuestion1);
-    $("#books").after(movieQuestion1options);
     $("#movies").html(movieQuestion2);
-    $("#movies").after(movieQuestion2options);
     $("#people").html(movieQuestion3);
-    $("#people").after(movieQuestion3options);
     $("#songs").html(movieQuestion4);
-    $("#songs").after(movieQuestion4options);
   });
   });
 
@@ -47,6 +46,7 @@ $(document).ready(function() {
 
 //If the 'People' button is pressed, do the following 
 $(document).ready(function() {
+  $("ul").hide();
   $("#people").click(function(){
     $("#p2").hide();
     $("#books").hide();
@@ -58,6 +58,7 @@ $(document).ready(function() {
 
 //If the 'Songs' button is pressed, do the following 
 $(document).ready(function() {
+  $("ul").hide();
   $("#songs").click(function(){
     $("#p2").hide();
     $("#books").hide();
